ed33a00840d6531a8546e5e474f38c445370cdc8
pachipalaaravindyadav31-droid <pachipalaaravindyadav31@gmail.com>
2025-12-18 10:27:42 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+public class addition {
+    public static void main (String[] args)
+	{
+	    int a=21;
+	    int b=40;
+	    int sum=a+b;
+	   
+	    System.out.println("sum of two numbers:"+ sum);
+	}
+	
+}
